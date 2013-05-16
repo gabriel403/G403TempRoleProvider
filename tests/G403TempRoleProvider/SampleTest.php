55ab11f6ea6ecb4a9de1da04f8e578ff6aa49718
Gabriel Baker <gabriel@autonomicpilot.co.uk>
2013-05-16 10:38:45 +0100

    Initial commit

---
+++ tests/G403TempRoleProvider/SampleTest.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace G403TempRoleProviderTest;
+
+class SampleTest extends Framework\TestCase
+{
+
+    public function testSample()
+    {
+        $this->assertInstanceOf('Zend\Di\LocatorInterface', $this->getLocator());
+    }
+}
